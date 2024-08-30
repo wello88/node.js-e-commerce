9625f6a19e7cceaf49b399f4b10d025732f94988
abdoww <abdow88988@gmaill.com>
2024-08-30 15:38:36 +0300

    .

--- src/bootStrap.js
+++ src/bootStrap.js
@@ -1,47 +1,44 @@
-import { connectDB } from "../db/connection.js"
-// import adminRouter from "./modules/admin/admin.router.js"
-// import authRouter from "./modules/auth/auth.router.js"
-// import brandRouter from "./modules/brand/brand.router.js"
-// import categoryRouter from "./modules/category/category.router.js"
-// import productRouter from "./modules/product/product.router.js"
-// import reviewRouter from "./modules/review/review.router.js"
-// import subCategoryRouter from "./modules/subCategory/subCategory.router.js"
-// import wishlistRouter from "./modules/wishlist/wishlist.router.js"
 import Stripe from "stripe";
+import express from "express"
+import path from "path"
+import dotenv from 'dotenv'
+import { connectDB } from "../db/connection.js"
 import { Cart, Order, Product } from "../db/index.js";
 import { asyncHandler } from "./utils/apperror.js"
 import * as allRouters from './index.js'
 import { globalErrorHandler } from "./utils/apperror.js"
-import express from "express"
-import path from "path"
-import dotenv from 'dotenv'
 
 dotenv.config({ path: path.resolve('./config/.env') })
 
-export const webhook = asyncHandler(
-    async (req, res) => {
-        const sig = req.headers['stripe-signature'].toString()
-        const stripe = new Stripe(process.env.STRIPE_KEY)
-        let event = stripe.webhooks.constructEvent(req.body, sig,'whsec_vfvnEzZMRniDnjSpA2wGDKsQqt2muljs');
-
-
-        // Handle the event checkout.session.completed
-        if (event.type === 'checkout.session.completed') {
-            const checkout = event.data.object
-            //clear cart
-            const orderId = checkout.metadata.orderId
-            //update order status to placed
-            const orderExist = await Order.findByIdAndUpdate(orderId, { status: 'placed' }, { new: true })
-            const cart = await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
-            for(const product of orderExist.products){
-             await Product.findById(product.productId, { $inc: { stock: -product.quantity } })
-            }
-        }
-        // return a 200 res to acknowledge receipt of the event
-        res.send()
-    })
 
 export const bootStrap = (app) => {
+    app.post('/webhook',express.raw({type: 'application/json'}),
+        asyncHandler(
+            async (req, res) => {
+                const sig = req.headers['stripe-signature'].toString()
+                const stripe = new Stripe(process.env.STRIPE_KEY)
+                let event = stripe.webhooks.constructEvent(req.body, sig,'whsec_vfvnEzZMRniDnjSpA2wGDKsQqt2muljs');
+        
+        
+                // Handle the event checkout.session.completed  
+                if (event.type === 'checkout.session.completed') {
+                    const checkout = event.data.object  
+                    //clear cart
+                    const orderId = checkout.metadata.orderId
+                    //update order status to placed
+                    const orderExist = await Order.findByIdAndUpdate(orderId, { status: 'placed' }, { new: true })
+                    const cart = await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
+                    for(const product of orderExist.products){
+                     await Product.findById(product.productId, { $inc: { stock: -product.quantity } })
+                    }
+                }
+                // return a 200 res to acknowledge receipt of the event
+                res.send()
+            })
+    
+    
+    )
+    
     app.use('/uploads', express.static('uploads'))
     app.use(express.json())
     connectDB()
