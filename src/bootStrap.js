6dcdf5e455e0d8ae1524e037d078a45d6b855d29
abdoww <abdow88988@gmaill.com>
2024-08-30 15:55:57 +0300

    aa

--- src/bootStrap.js
+++ src/bootStrap.js
@@ -1,24 +1,18 @@
-import Stripe from "stripe";
-import express from "express"
 import path from "path"
+import { webhook } from "./utils/webhook.js";
 import dotenv from 'dotenv'
 import { connectDB } from "../db/connection.js"
-import { Cart, Order, Product } from "../db/index.js";
-import { asyncHandler } from "./utils/apperror.js"
 import * as allRouters from './index.js'
 import { globalErrorHandler } from "./utils/apperror.js"
-import { webhook } from "./utils/webhook.js";
 
 dotenv.config({ path: path.resolve('./config/.env') })
 
 
 export const bootStrap = (app,express) => {
-    app.post('/webhook',express.raw({type: 'application/json'}),
- 
-    webhook
-    
-    )
-    
+    app.post('/webhook',
+        express.raw({ type: 'application/json' }),
+        webhook
+      );
     app.use('/uploads', express.static('uploads'))
     app.use(express.json())
     connectDB()
